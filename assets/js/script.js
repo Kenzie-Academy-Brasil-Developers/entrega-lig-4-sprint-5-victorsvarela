6ed4254c47932fc1ea8999a57d7bab9c1b8f401b
Victor Varela <vitaotapioqueiro@gmail.com>
2021-08-09 19:35:39 -0300

    add images

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const movimento = document.sndfaskldfafasd
+
+const renderGame = () => {
+    // fazer toda a estrutura do jogo
+}
+
+const movementPills = () =>{
+    // fazer a movimentação das pílulas para a coluna selecionado pelo click
+}
+
+
+const startGame = () => {
+
+}
+
+startGame()
