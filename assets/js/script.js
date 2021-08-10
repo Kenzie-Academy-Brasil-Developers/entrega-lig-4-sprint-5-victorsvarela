bad712121c103a134205c53f80256a94ad5ecb98
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2021-08-09 21:06:52 -0300

    criado a função render

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,38 @@
-const movimento = document.sndfaskldfafasd
+let gameStructure = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+console.log(gameStructure);
+// Criar função start game
 
-const renderGame = () => {
-    // fazer toda a estrutura do jogo
-}
-
-const movementPills = () =>{
-    // fazer a movimentação das pílulas para a coluna selecionado pelo click
-}
-
-
+let array = [1,2,3,4,5,6,7]
 const startGame = () => {
 
 }
 
-startGame()
+startGame();
+
+// Criar função de renderizar;
+
+const renderGame = () => {
+    const container = document.getElementById("gameScreen");
+    for (let i = 0; i < gameStructure.length; i++) {
+        let column = document.createElement("div");
+        column.id = `${i}`; 
+        column.classList.add("coluna")
+        for (let j = 0; j < gameStructure[i].length; j++) {
+            let row = document.createElement("div");
+            row.id =  `${i}${j}`
+            row.classList.add("linha");
+            column.appendChild(row);
+            container.appendChild(column);
+        }
+    }
+}
+
+renderGame();
