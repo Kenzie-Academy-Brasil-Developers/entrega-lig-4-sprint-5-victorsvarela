e98c1cabacf7b12c5d7c0b338e06a71ff585943e
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2021-08-09 21:20:01 -0300

    ajuste da função render. Removido transforme e inserido display flex na classe .gameStation

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,12 @@ let gameStructure = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0]
-]
+];
+
 console.log(gameStructure);
 // Criar função start game
 
-let array = [1,2,3,4,5,6,7]
+
 const startGame = () => {
 
 }
@@ -24,10 +25,10 @@ const renderGame = () => {
     for (let i = 0; i < gameStructure.length; i++) {
         let column = document.createElement("div");
         column.id = `${i}`; 
-        column.classList.add("coluna")
+        column.classList.add("coluna");
         for (let j = 0; j < gameStructure[i].length; j++) {
             let row = document.createElement("div");
-            row.id =  `${i}${j}`
+            row.id =  `${i}${j}`;
             row.classList.add("linha");
             column.appendChild(row);
             container.appendChild(column);
