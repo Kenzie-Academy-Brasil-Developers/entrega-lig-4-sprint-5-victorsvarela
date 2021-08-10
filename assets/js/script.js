89953028171a77f83beac589e679d229d4b9b606
Gustavo Lira <gustavolira8813@gmail.com>
2021-08-10 17:36:04 -0300

    funcionalidades dos btn regras/voltar adicionadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
-let btnBackRules = document.getElementById("backRules")
-let rulesSection = document.getElementById("rulesScreen")
+const btnBackRules = document.getElementById("backRules")
+const rulesSection = document.getElementById("rulesScreen")
+const btnRules = document.getElementById("btnRules")
+
+function rulesScreen(){
+    rulesSection.classList.remove('hidden')
+    btnBackRules.classList.remove("hidden")
+}
+function backRules(){
+    rulesSection.classList.add('hidden')
+    btnBackRules.classList.add("hidden")
+
+}
+btnRules.addEventListener('click', (e)=>{
+    rulesScreen()
+         
+})
 
 btnBackRules.addEventListener('click', (e) =>{
     backRules()
 })
-function backRules(){
-    rulesSection.classList.add('hidden')
-}
+
 
 
 
