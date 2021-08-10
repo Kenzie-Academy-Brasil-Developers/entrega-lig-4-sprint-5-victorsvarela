870dbbb008eab1a4f64f1e97551f1d2bcf488c7e
Victor Varela <vitaotapioqueiro@gmail.com>
2021-08-10 17:50:11 -0300

    tela nomes players ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
+const btnPlay = document.getElementById("btnPlay");
+const selectNameSection = document.getElementById('selectNameScreen');
+const btnInitialScreen = document.getElementById('btnInitialScreen')
 let btnBackRules = document.getElementById("backRules")
 let rulesSection = document.getElementById("rulesScreen")
+
+const showSelectNamePlayers = (event) => {
+    selectNameSection.classList.remove('hidden')
+}
+
+const hide = (event) => {
+    selectNameSection.classList.add('hidden')
+}
+
+btnPlay.addEventListener('click', showSelectNamePlayers)
+btnInitialScreen.addEventListener('click', hide)
 
 btnBackRules.addEventListener('click', (e) =>{
     backRules()
